a5ebc4394eb27e0d0ba251f959a0ac09b89f6684
nathan78906 <nathen78906@gmail.com>
2018-06-29 17:49:25 -0400

    update user-agent to follow heroku TOS

--- saliens.py
+++ saliens.py
@@ -8,7 +8,7 @@ TOKEN = ""
 
 s = requests.session()
 s.headers.update({
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
+    'User-Agent': 'steam-salien-cheat on GitHub Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
     'Referer': 'https://steamcommunity.com/saliengame/play/',
     'Origin': 'https://steamcommunity.com',
     'Accept': '*/*'
